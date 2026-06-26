873c0e6ac34b15b207cd584a005c674a87d87925
Vitoria Cristina <vitoria.reis14@escola.pr.gov.br>
2026-06-26 10:00:25 -0300

    Fix formatting issues in main.js

--- main.js
+++ main.js
@@ -74,3 +74,8 @@ function classificaSenha(tamanhoAlfabeto) {
     }
     const valorEntropia = document.querySelector('.entropia');
     valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + " dias para descobrir essa senha.";
+}
+
+
+
+
